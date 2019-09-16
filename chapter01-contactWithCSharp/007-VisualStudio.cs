65f1f90e85e0a9208e4c0d57586b688570a794bf
Nacho Cabanes <nacho@nachocabanes.com>
2019-09-16 23:06:35 +0200

    007 - Contact with Visual Studio

---
+++ chapter01-contactWithCSharp/007-VisualStudio.cs
@@ -0,0 +1,11 @@
+using System;
+
+class VisualStudioExample
+{
+    static void Main()
+    {
+        Console.WriteLine("125 yards are");
+        Console.WriteLine(125 * 36);
+        Console.WriteLine("inches");
+    }
+}
