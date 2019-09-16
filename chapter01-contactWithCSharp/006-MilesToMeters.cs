7ff13c46af46f7d989faae37035603ff7effcb68
Nacho Cabanes <nacho@nachocabanes.com>
2019-09-16 23:06:02 +0200

    007 - 25 miles to meters

---
+++ chapter01-contactWithCSharp/006-MilesToMeters.cs
@@ -0,0 +1,11 @@
+using System;
+
+class MilesToMeters
+{
+    static void Main()
+    {
+        Console.WriteLine("25 miles are");
+        Console.WriteLine(25 * 1609);
+        Console.WriteLine("meters");
+    }
+}
