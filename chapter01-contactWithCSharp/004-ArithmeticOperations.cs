6a1021c8155173015fa57c6c255bfe7f2950be4d
Nacho Cabanes <nacho@nachocabanes.com>
2019-09-16 19:46:22 +0200

    004 - Arithmetic operations

---
+++ chapter01-contactWithCSharp/004-ArithmeticOperations.cs
@@ -0,0 +1,14 @@
+class ArithmeticOperations
+{
+    static void Main() 
+    {
+        System.Console.WriteLine(14+4); 
+        System.Console.WriteLine(14-4); 
+        System.Console.WriteLine(14*4); 
+
+        System.Console.WriteLine(14/4); 
+        System.Console.WriteLine(14%4); 
+
+        System.Console.WriteLine(14.0/4); 
+    }
+}
