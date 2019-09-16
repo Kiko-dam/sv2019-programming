e2d7bee345fa42f3f03c1326f07e28204681e580
Nacho Cabanes <nacho@nachocabanes.com>
2019-09-16 19:48:29 +0200

    005 - Using System

---
+++ chapter01-contactWithCSharp/005-UsingSystem.cs
@@ -0,0 +1,16 @@
+using System;
+
+class UsingSystem
+{
+    static void Main() 
+    {
+        Console.WriteLine(14+4); 
+        Console.WriteLine(14-4); 
+        Console.WriteLine(14*4); 
+
+        Console.WriteLine(14/4); 
+        Console.WriteLine(14%4); 
+
+        Console.WriteLine(14.0/4); 
+    }
+}
