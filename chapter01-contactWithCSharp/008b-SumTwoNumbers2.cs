bbd6184c61755294344301de3f3e1fd608f9fb3c
Nacho Cabanes <nacho@nachocabanes.com>
2019-09-17 09:37:12 +0200

    008b - Sum Two numbers (b: improved version)

---
+++ chapter01-contactWithCSharp/008b-SumTwoNumbers2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class SumTwoNumbers2
+{
+    static void Main()
+    {
+        int a, b, sum;
+        
+        Console.Write("Enter a number: ");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter another number: ");
+        b = Convert.ToInt32(Console.ReadLine());
+        
+        sum = a + b;
+        Console.Write("Their sum is: ");
+        Console.WriteLine(sum);
+    }
+}
