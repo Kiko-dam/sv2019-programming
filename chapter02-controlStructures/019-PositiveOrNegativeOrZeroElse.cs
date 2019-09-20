a0f9710d24cb5f947ac2147569563a32c6ecf526
Nacho Cabanes <nacho@nachocabanes.com>
2019-09-20 11:53:22 +0200

    019 - Positive, negative or zero (V2: else)

---
+++ chapter02-controlStructures/019-PositiveOrNegativeOrZeroElse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class PositiveOrNegativeOrZero
+{
+    static void Main()
+    {
+        int number;
+
+        Console.Write("Enter a number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        if (number > 0)
+            Console.WriteLine("The number is positive.");
+        else if (number < 0)
+            Console.WriteLine("The number is negative.");
+        else
+            Console.WriteLine("The number is zero.");
+    }
+}
+
