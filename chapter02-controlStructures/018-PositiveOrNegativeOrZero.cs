afb98c64908f080ba0e9e4bb841082a0b9f673f3
Nacho Cabanes <nacho@nachocabanes.com>
2019-09-20 11:52:03 +0200

    018 - Positive, negative or zero (first approach)

---
+++ chapter02-controlStructures/018-PositiveOrNegativeOrZero.cs
@@ -0,0 +1,23 @@
+﻿// Diego Aníbal Lezcano Reissner
+using System;
+
+class PositiveOrNegativeOrZero
+{
+    static void Main()
+    {
+        int number;
+
+        Console.Write("Enter a number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        if (number > 0)
+            Console.WriteLine("The number is positive.");
+
+        if (number < 0)
+            Console.WriteLine("The number is negative.");
+
+        if (number == 0)
+            Console.WriteLine("The number is zero.");
+    }
+}
+
