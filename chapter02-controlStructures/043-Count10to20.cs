cc86f153df13cda557f0b1ac266624c32c70277b
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-09-26 16:58:13 +0200

    043 - Count 10 to 20

---
+++ chapter02-controlStructures/043-Count10to20.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Count10to20
+{
+    static void Main()
+    {
+        int i = 10;
+
+        while (i <= 20)
+        {
+            Console.Write("{0} ",i);
+            i = i + 1;
+        }
+    }
+}
+
