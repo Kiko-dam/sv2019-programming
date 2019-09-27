7d1b2f04f5715d2f9f8b35f148f2b145bfe088da
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-09-27 17:00:54 +0200

    050 - Absolute value, ternary operator

---
+++ chapter02-controlStructures/050-AbsoluteValue.cs
@@ -0,0 +1,23 @@
+// Absolute Value
+
+using System;
+class AbsoluteValue
+{
+    static void Main()
+    {
+        int x, abs;
+        
+        Console.Write("Enter a number: ");
+        x = Convert.ToInt32( Console.ReadLine() );
+        
+        if (x >= 0)
+            abs = x;
+        else
+            abs = -x;
+        Console.WriteLine("Its absolute value is {0}", abs);
+        
+        abs = x >= 0 ? x : -x;
+        Console.WriteLine("Its absolute value is {0}", abs);
+    }
+}
+
