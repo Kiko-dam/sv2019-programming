6b4bdbb4f09e91fb7215f7ef60a0233d6512c40b
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-09-30 16:39:40 +0200

    053 - Count 10 to 20 (for)

---
+++ chapter02-controlStructures/053a-Count10to20For1.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Count10to20For
+{
+    static void Main()
+    {
+        int i;
+
+        for (i = 10; i <= 20 ; i = i + 1)
+        {
+            Console.Write("{0} ",i);
+        }
+    }
+}
+
---
+++ chapter02-controlStructures/053b-Count10to20For2.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Count10to20For
+{
+    static void Main()
+    {
+        for (int i = 10; i <= 20 ; i++)
+            Console.Write("{0} ",i);
+    }
+}
+
