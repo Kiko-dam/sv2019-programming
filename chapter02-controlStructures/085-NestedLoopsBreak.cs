01295406e5e3e467aa6e669e5f1db3b1d96a7559
Nacho Cabanes <nacho@nachocabanes.com>
2019-10-07 22:20:59 +0200

    085 - Nested loops + break

---
+++ chapter02-controlStructures/085-NestedLoopsBreak.cs
@@ -0,0 +1,24 @@
+//Pablo Conde
+//goto bucles anidados
+
+using System;
+
+class GotoBuclesAnidados
+{
+     static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 20; j <= 30; j = j+2)
+            {
+                if (i == 3 && j > 25)
+                {
+                    Console.WriteLine("Tryng to break...");
+                    break;
+                }
+            
+                Console.WriteLine("{0} - {1} ", i, j);
+            }
+        }
+    }                                                                 
+}
