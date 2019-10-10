85b379275afcf537cd1bf9660c4fa999b3c45385
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-10-10 16:06:07 +0200

    088 - Power

---
+++ chapter02-controlStructures/088-Power.cs
@@ -0,0 +1,21 @@
+﻿// Diego Aníbal Lezcano Reissner y Lautaro Álvaro Fernández
+
+using System;
+
+class Exponent
+{
+    static void Main()
+    {
+        Console.Write("Enter base: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter exponent: ");
+        int exp = Convert.ToInt32(Console.ReadLine());
+
+        int result = 1;
+        for (int i = 0; i < exp; i++)
+        {
+            result = result * n;
+        }
+        Console.WriteLine("{0} ^ {1} = {2}", n, exp, result);
+    }
+}
