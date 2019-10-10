63fa464347ade0771fc0d694e6c3e98cbafbd3c6
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-10-10 16:08:30 +0200

    089 - Contact with exceptions

---
+++ chapter02-controlStructures/089-Exceptions1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class SumTwoNumbersExceptions
+{
+    static void Main()
+    {
+        int a, b, sum;
+        
+        try
+        {
+            Console.Write("Enter a number: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            
+            sum = a + b;
+            Console.Write("Their sum is: ");
+            Console.WriteLine(sum);
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("That is not a valid number");
+        }
+        
+        Console.WriteLine("Have a nice day!");
+    }
+}
