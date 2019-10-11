2d2b2e104845abca22c5ba4309167e584c1e859c
Nacho Cabanes <ncabanes@users.noreply.github.com>
2019-10-11 16:58:34 +0200

    096 - Try/catch 2 (multiple exceptions)

---
+++ chapter02-controlStructures/096-TryCatch2.cs
@@ -0,0 +1,31 @@
+﻿// Diego Aníbal Lezcano Reissner
+
+using System;
+
+class TryCatch
+{
+    static void Main()
+    {
+        int n1=0, n2=0;
+        try
+        {
+            Console.Write("Enter a number: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} / {1} = {2}", n1, n2, n1/n2);
+            Console.WriteLine("{0} x {1} = {2}", n1, n2, n1*n2);
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by 0.");
+            Console.WriteLine("But {0} x {1} = {2}", n1, n2, n1*n2);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid format");
+        }
+
+    }
+}
