dd67a845fb2b9439d06d77d9d82c0fff3c78bce5
Nacho Cabanes <nacho@nachocabanes.com>
2019-10-17 20:05:38 +0200

    103 - Multiple counts (two variables in "for")

---
+++ chapter02-controlStructures/103-MultipleCounts.cs
@@ -0,0 +1,12 @@
+//Pablo Miguel Climent Gonzálvez
+using System;
+class MultipleCounts
+{
+    static void Main()
+    {
+        for(int i=1, j=20 ; i<=10 && j<=30 ; i++,j+=2)
+        {
+            Console.WriteLine("i = {0} & j = {1}",i,j);
+        }
+    }
+}
