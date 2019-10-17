f04b075887e81109e038b90d96dea89d34122b86
Nacho Cabanes <nacho@nachocabanes.com>
2019-10-17 20:20:23 +0200

    Pong 001 - Display a bat

---
+++ projects/pong/versions/pong-001.cs
@@ -0,0 +1,14 @@
+// Versión  Fecha     Por + cambios
+// -------  --------  ------------------------------------
+// 0.01     17-10-19  Joel Martinez: Display a bat
+
+using System;
+
+class pong
+{
+    static void Main()
+    {
+        Console.SetCursorPosition(77, 12);
+        Console.Write("|");
+    }
+}
