b8aae3466eef19f59e774cf4b9cb6127815b62bc
Nacho Cabanes <nacho@nachocabanes.com>
2019-10-18 22:26:48 +0200

    105 - Increment

---
+++ chapter03-dataTypes/105-Increment.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Increment
+{
+    static void Main()
+    {
+        int a, b, c, d;
+        a = 5;
+        a++;
+        a *= 2;
+        b = 13;
+        b %= 6;
+        b -= 7;
+        b--;
+        c = b++;
+        d = ++c;
+    }
+}
