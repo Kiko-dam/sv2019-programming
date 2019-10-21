08337c511c1ef6b4e7c3cb9c4a242181080337f9
Nacho Cabanes <nacho@nachocabanes.com>
2019-10-21 19:37:27 +0200

    114 - char + for

---
+++ chapter03-dataTypes/114a-CharsFor1.cs
@@ -0,0 +1,21 @@
+﻿// Daniel Contreras
+// char + for
+
+using System;
+
+class CharsAndFor
+{
+    public static void Main()
+    {
+        char letter;
+
+        Console.Write("Enter an uppercase letter: ");
+        letter = Convert.ToChar(Console.ReadLine());
+
+        for (char c = letter; c <= 'Z'; c++)
+        {
+            Console.Write("{0} ", c);
+        }
+    }
+}
+
---
+++ chapter03-dataTypes/114b-CharsFor2.cs
@@ -0,0 +1,26 @@
+﻿// Daniel Contreras
+// char + for
+
+using System;
+
+class CharsAndFor
+{
+    public static void Main()
+    {
+        char letter;
+
+        do
+        {
+            Console.Write("Enter an uppercase letter: ");
+            letter = Convert.ToChar(Console.ReadLine());
+        }
+        while (letter < 'A' || letter > 'Z');
+    
+
+        for (char c = letter; c <= 'Z'; c++)
+        {
+            Console.Write("{0} ", c);
+        }
+    }
+}
+
